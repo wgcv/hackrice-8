9e5e70fbb6df67643f228079f3c26613530f2a05
Gustavo Cevallos <gstavocevallos@gmail.com>
2018-09-16 00:52:10 -0500

    update size of amount

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class DataUser(models.Model):
     progress = models.IntegerField(default = 0)
     next_progress = models.IntegerField(default = 40)
     level = models.IntegerField(default = 1)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, default = 0)
+    amount = models.DecimalField(max_digits=30, decimal_places=2, default = 0)
     def __str__(self):
         return self.user.username
 transaction_type = (
@@ -22,7 +22,7 @@ transaction_type = (
 
 )
 class Transaction(models.Model):
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=30, decimal_places=2)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     type_transaction = models.CharField(max_length=2, choices=transaction_type)
